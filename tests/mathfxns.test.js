b8415d31aa90184331ef82f0fbb3d2a7a45a3e30
smbudhwani <smbudhwani@yahoo.com>
2021-05-18 09:50:08 -0500

    5 tests done tody

--- tests/mathfxns.test.js
+++ tests/mathfxns.test.js
@@ -1,5 +1,5 @@
 const { assert, expect } = require('chai');
-const { describe, it } = require('mocha');
+//const { describe, it } = require('mocha');
 const MathFxns = require('../mathfxns.js');
 
 describe('math functions \n', () => {
@@ -11,7 +11,58 @@ describe('math functions \n', () => {
       const sum = 16;
       assert.ok(math.addTwoNums(num1, num2) === sum);
     });
+
   });
+  describe('subtractTwoNums', () => {
+    it('should return the difference of two numbers', () => {
+      //setup
+      const math = new MathFxns();
+      const num1 = 16;
+      const num2 = 11;
+      //excercise
+      const diff = 5;
+      //verify
+      assert.ok(math.subtractTwoNums(num1, num2) === diff);
+    });
+
+  });
+  describe('multiplyTwoNums', () => {
+    it('should return product of two numbers', () => {
+      //setup
+      const math = new MathFxns();
+      const num1 = 5;
+      const num2 = 11;
+      //excercise
+      const multiply = 55;
+      //verify
+      assert.ok(math.multiplyTwoNums(num1, num2) === multiply);
+    });
+
+  });
+  describe('divideTwoNums', () => {
+    it('should return division of two numbers', () => {
+      //setup
+      const math = new MathFxns();
+      const num1 = 50;
+      const num2 = 5;
+      //excercise
+      const division = 10;
+      //verify
+      assert.ok(math.divideTwoNums(num1, num2) === division);
+    });
+  });
+  describe('addAllNums', () => {
+    it('should return sum of any numbers', () => {
+      //setup
+      const math = new MathFxns();
+      const val = [2,4,5,8,9];
+      //excercise
+      sum = 28;
+      //verify
+      assert.ok(math.addAllNums(...val) === sum);
+    });
+  });
+  
   // ...
   // add more tests here
 });
