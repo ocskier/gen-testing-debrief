46a4dc88fa4bd2493c66f0c78feb611754a9c0d1
Jon Jackson <ocskier@gmail.com>
2021-05-17 22:18:56 -0400

    feat: add comments for activity

--- mathfxns.js
+++ mathfxns.js
@@ -19,11 +19,11 @@ class MathFxns {
   }
   // takes two arguments returns the product
   multiplyTwoNums(num1, num2) {
-    return num1 * num2;
+    return num1 ** num2;
   }
   // takes two arguments and divides them returning the sum
   divideTwoNums(num1, num2) {
-    return num1 / num2;
+    return num1 % num2;
   }
   // takes any number of numbers and adds all together returning the sum
   addAllNums(...rest) {
