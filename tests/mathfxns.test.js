b55b7b0faf5419a6fec6bf483d0b28ca84da7149
Ashley U <ashley.umetsu@gmail.com>
2021-05-18 09:17:28 -0500

    testing questions

--- tests/mathfxns.test.js
+++ tests/mathfxns.test.js
@@ -12,6 +12,50 @@ describe('math functions \n', () => {
       assert.ok(math.addTwoNums(num1, num2) === sum);
     });
   });
-  // ...
-  // add more tests here
+  it('should return a string', () => {
+    const math = new MathFxns();
+    const num1 = 5;
+    const num2 = 11;
+    const sum = 16;
+    assert.ok(math.addTwoNums(num1, num2) == '16');
+  });
+  it('subtracts two numbers', () => {
+    const math = new MathFxns();
+    const num1 = 11;
+    const num2 = 5;
+    const subtract = 6;
+    assert.ok(math.subtractTwoNums(num1, num2) === subtract);
+  });
+  it('multiplies two numbers', () => {
+    const math = new MathFxns();
+    const num1 = 11;
+    const num2 = 5;
+    const multiply = 55;
+    assert.ok(math.multiplyTwoNums(num1, num2) === multiply);
+  });
+  it('divides two numbers', () => {
+    const math = new MathFxns();
+    const num1 = 10;
+    const num2 = 5;
+    const divide = 2;
+    assert.ok(math.divideTwoNums(num1, num2) === divide);
+  });
+  it('Adds all numbers, returns sum', () => {
+    const math = new MathFxns();
+    const num1 = 10;
+    const num2 = 5;
+    const num3 = 5;
+    const sum = 20;
+    assert.ok(math.addAllNums(num1, num2, num3) === sum);
+  });
+  it('Check if a number is prime', () => {
+    const math = new MathFxns();
+    const num = 13;
+    assert.ok(math.isPrime(num));
+  });
+  it('return an array of factors', () => {
+    const math = new MathFxns();
+    const num = 5;
+    assert.ok(math.factor(num));
+  });
 });
