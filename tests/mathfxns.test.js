893942ef0441bbfa786e15dfd8a5b3fa666e015b
Jon Jackson <ocskier@gmail.com>
2021-05-18 08:56:37 -0400

    fix: add mocha imports

--- tests/mathfxns.test.js
+++ tests/mathfxns.test.js
@@ -1,11 +1,11 @@
 const { assert, expect } = require('chai');
 const { describe, it } = require('mocha');
 const MathFxns = require('../mathfxns.js');
-const math = new MathFxns();
 
 describe('math functions \n', () => {
   describe('addTwoNums', () => {
     it('should return the sum of two numbers', () => {
+      const math = new MathFxns();
       const num1 = 5;
       const num2 = 11;
       const sum = 16;
